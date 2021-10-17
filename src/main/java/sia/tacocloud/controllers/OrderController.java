5587a29903c24f2de5560a5c2813018a5b3c3953
Teodoro Dawn <teodawn.td@gmail.com>
2021-10-17 12:29:00 -0500

    New feature: Order view and controller
    The template and file for requestign an order of tacos is alredy done.
    
    To do:
       it has a bug and the view doesn't work yey

---
+++ src/main/java/sia/tacocloud/controllers/OrderController.java
@@ -0,0 +1,20 @@
+package sia.tacocloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// import lombok.extern.slf4j.Slf4j;
+import sia.tacocloud.models.TacoOrder;
+
+// @Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("tacoOrder", new TacoOrder());
+        return "orderForm";
+    }
+}
